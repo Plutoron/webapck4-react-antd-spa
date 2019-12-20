62099e6d98ad2df9afbd4e18b5d6dfbebae56730
yunlong.syl <yunlong.syl@dtwave-inc.com>
2019-12-20 10:08:46 +0800

    add babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin') // 删除 旧的�
 const safeParser = require('postcss-safe-parser') // 添加前缀的规则
 
 const HOST = '127.0.0.1'
-const PORT = '8081'
+const PORT = '8080'
 
 module.exports = (env, argv) => {
   const { 
